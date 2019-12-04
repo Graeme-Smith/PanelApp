75dafa72184ae7d6124d1cdc55eb77af4d590527
Graeme Smith <graeme_c_smith@hotmail.com>
2019-12-04 15:30:52 +0000

    Initial WebGesaltR script

---
+++ webGestaltAPI.R
@@ -0,0 +1,11 @@
+
+# Interface with the WebGestalt API
+
+geneFile <- system.file("extdata", "interestingGenes.txt", package="WebGestaltR")
+refFile <- system.file("extdata", "referenceGenes.txt", package="WebGestaltR")
+outputDirectory <- getwd()
+enrichResult <- WebGestaltR(enrichMethod="ORA", organism="hsapiens",
+                            enrichDatabase="pathway_KEGG", interestGeneFile=geneFile,
+                            interestGeneType="genesymbol", referenceGeneFile=refFile,
+                            referenceGeneType="genesymbol", isOutput=TRUE, outputDirectory=outputDirectory,
+                            projectName=NULL)
