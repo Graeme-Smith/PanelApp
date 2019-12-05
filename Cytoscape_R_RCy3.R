d8caf23a56398ace9e14aecde66330ce8ce28f03
unknown <callum_rakhit@hotmail.com>
2019-12-05 10:22:30 +0000

    Working functions to expand gene list, create network, create image of network.

--- Cytoscape_R_RCy3.R
+++ Cytoscape_R_RCy3.R
@@ -1,88 +1,99 @@
-install.packages("BiocManager")
-BiocManager::install("RCy3")
-library(RCy3)
-library(igraph)
-
-cytoscapePing ()
-
-cytoscapeVersionInfo ()
-
-#list of cytoscape apps to install
-installation_responses <- c()
-
-#list of app to install
-installation_responses <- c()
-cyto_app_toinstall <- c("clustermaker2", "enrichmentmap", "autoannotate", "wordcloud", "stringapp", "aMatReader")
-cytoscape_version <- unlist(strsplit( cytoscapeVersionInfo()['cytoscapeVersion'],split = "\\."))
-
-if(length(cytoscape_version) == 3 && as.numeric(cytoscape_version[1]>=3) 
-   && as.numeric(cytoscape_version[2]>=7)){
-  for(i in 1:length(cyto_app_toinstall)){
-    #check to see if the app is installed.  Only install it if it hasn't been installed
-    if(!grep(commandsGET(paste("apps status app=\"", cyto_app_toinstall[i],"\"", sep="")), 
-             pattern = "status: Installed")){
-      installation_response <-commandsGET(paste("apps install app=\"", 
-                                                cyto_app_toinstall[i],"\"", sep=""))
-      installation_responses <- c(installation_responses,installation_response)
-    } else{
-      installation_responses <- c(installation_responses,"already installed")
-    }
-  }
-  installation_summary <- data.frame(name = cyto_app_toinstall, 
-                                     status = installation_responses)
-  
-  knitr::kable(list(installation_summary),
-               booktabs = TRUE, caption = 'A Summary of automated app installation'
-  )
+# Get required packages
+GetPackages <- function(required.packages) {
+  packages.not.installed <- required.packages[!(required.packages %in% installed.packages()[, "Package"])]
+  if(length(packages.not.installed)){install.packages(packages.not.installed, dependencies = T)}
+  suppressMessages(lapply(required.packages, require, character.only = T))
 }
 
+GetPackages(c("BiocManager"))
+
+# One package needed from BiocManager
+BiocManager::install("RCy3")
+Non.CRAN.packages <- c("RCy3", "igraph")
+lapply(Non.CRAN.packages, library, character.only = T)
+
+# Test if Cytoscape has successfully loaded
+test.message <- cytoscapePing ()
+if(test.message == "You are connected to Cytoscape!"){
+  print("You are connected to Cytoscape!")
+  } else {
+    print("Connection failed - make sure Cytoscape is open locally")
+    }
+
+# Can use cytoscapeVersionInfo () to get version information
+
+#list of cytoscape apps to install
+CytoscapeAppInstallation <- function(App.List){
+  # List of app to install
+  installation_responses <- c()
+  cytoscape_version <- unlist(strsplit( cytoscapeVersionInfo()['cytoscapeVersion'],split = "\\."))
+  if(length(cytoscape_version) == 3 && as.numeric(cytoscape_version[1]>=3) && as.numeric(cytoscape_version[2]>=7)) {
+    for(i in 1:length(App.List)){
+      # Check if the app is installed, only install if it hasn't been already
+      if(!grep(commandsGET(paste("apps status app=\"", App.List[i],"\"", sep="")), pattern = "status: Installed")) {
+        installation_response <- commandsGET(paste("apps install app=\"", App.List[i],"\"", sep=""))
+        installation_responses <- c(installation_responses,installation_response)
+        } else {
+          installation_responses <- c(installation_responses,"already installed")
+        }
+      } 
+    installation_summary <- data.frame(name = App.List, status = installation_responses)
+    knitr::kable(list(installation_summary), booktabs = TRUE, caption = 'A Summary of automated app installation') 
+    }
+  }
+
+App.List <- c("clustermaker2", "enrichmentmap", "autoannotate", "wordcloud", "stringapp", "aMatReader")
+CytoscapeAppInstallation(App.List)
+
+# This one didn't work for some unknown reason, so do it manually
 installApp("stringApp")
 
-commandsHelp("help")
+panel.genes <- read.csv(file = "../../Documents/genelist_example.csv")
 
-genes <- read.csv(file = "../../Documents/genelist_example.csv")
+StringGeneExpansion <- function(panel.genes){
+  string_interaction_cmd <- paste('string protein query taxonID=9606 limit=150 cutoff=0.9 query="',
+                                  paste(panel.genes$geneid, collapse = ","), '"', sep = "")
+  commandsGET(mesen_string_interaction_cmd)
+  node.attribute.table <- getTableColumns(table="node")
+  return(node.attribute.table)
+}
 
-mesen_string_interaction_cmd <- paste('string protein query taxonID=9606 limit=150 cutoff=0.9 query="',
-                                      paste(genes$geneid, collapse = ","), '"', sep = "")
+node.attribute.table <- StringGeneExpansion(panel.genes)
 
-commandsGET(mesen_string_interaction_cmd)
-
-node_attribute_table_topmesen <- getTableColumns(table="node")
-node_attribute_table_topmesen$`display name`
-
-# mesen_string_interaction_cmd_2 <- paste('string protein query taxonID=9606 limit=150 cutoff=0.9 query="',
-#                                       paste(node_attribute_table_topmesen$`display name`, collapse = ","), 
-#                                       '"', sep = "")
-# 
-# commandsGET(mesen_string_interaction_cmd_2)
-# 
-# node_attribute_table_topmesen <- getTableColumns(table="node")
-# node_attribute_table_topmesen$`display name`
+# Get the gene names from the network
+expanded.gene.list <- node.attribute.table$`display name`
 
 # Create network style
 style.name = "TestStyle"
-defaults.list <- list(NODE_SHAPE = "ellipse",
-                      NODE_SIZE = 10,
-                      NODE_FILL_COLOR = "#AAAAAA",
-                      EDGE_TRANSPARENCY = 120)
+defaults.list <- list(
+  NODE_SHAPE = "ellipse", NODE_SIZE = 10, NODE_FILL_COLOR = "#AAAAAA", EDGE_TRANSPARENCY = 120)
 node.label.map <- mapVisualProperty('node label', 'display name', 'p') # p for passthrough; nothing else needed
 createVisualStyle(style.name, defaults.list, list(node.label.map))
 setVisualStyle(style.name=style.name)
 
 # Create force directed layout
-layoutNetwork('force-directed')
-getLayoutPropertyNames(layout.name = 'force-directed')
-layoutNetwork('force-directed defaultSpringCoefficient=0.0000008 defaultSpringLength=70')
+NetworkImage <- function(){
+  layoutNetwork('force-directed')
+  getLayoutPropertyNames(layout.name = 'force-directed')
+  layoutNetwork('force-directed defaultSpringCoefficient=0.0000008 defaultSpringLength=70')
+}
 
-# Save it
-relayout_string_network_png_file_name <- file.path(
-  getwd(), "relayout_string_network.png")
+NetworkImage()
+  
+SaveNetworkImage <- function(){
+  string_network_png_file_name <- file.path(getwd(), "string_network.png")
+  if(file.exists(string_network_png_file_name)){
+  # cytoscape hangs waiting for user response if file already exists, so remove if present
+  response <- file.remove(string_network_png_file_name)
+  } else { 
+    response <- exportImage(string_network_png_file_name, type = "png") }
+}
 
-if(file.exists(relayout_string_network_png_file_name)){
-  #cytoscape hangs waiting for user response if file already exists.  Remove it first
-  response<- file.remove(relayout_string_network_png_file_name)
-} 
+SaveNetworkImage()
 
-response <- exportImage(relayout_string_network_png_file_name, type = "png")
+
+
+
+
 
 
