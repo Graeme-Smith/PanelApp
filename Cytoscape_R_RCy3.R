8ef8c0d843c65d0888f2045d1e350877908cf0d5
unknown <callum_rakhit@hotmail.com>
2019-12-05 10:32:12 +0000

    Further comments and a minor bug fix

--- Cytoscape_R_RCy3.R
+++ Cytoscape_R_RCy3.R
@@ -48,12 +48,14 @@ CytoscapeAppInstallation(App.List)
 # This one didn't work for some unknown reason, so do it manually
 installApp("stringApp")
 
+# This needs to be passed from the PanelApp list
 panel.genes <- read.csv(file = "../../Documents/genelist_example.csv")
 
 StringGeneExpansion <- function(panel.genes){
+  # This determines the query sent to String to expand the gene list
   string_interaction_cmd <- paste('string protein query taxonID=9606 limit=150 cutoff=0.9 query="',
                                   paste(panel.genes$geneid, collapse = ","), '"', sep = "")
-  commandsGET(mesen_string_interaction_cmd)
+  commandsGET(string_interaction_cmd)
   node.attribute.table <- getTableColumns(table="node")
   return(node.attribute.table)
 }
@@ -65,11 +67,12 @@ expanded.gene.list <- node.attribute.table$`display name`
 
 # Create network style
 style.name = "TestStyle"
-defaults.list <- list(
-  NODE_SHAPE = "ellipse", NODE_SIZE = 10, NODE_FILL_COLOR = "#AAAAAA", EDGE_TRANSPARENCY = 120)
-node.label.map <- mapVisualProperty('node label', 'display name', 'p') # p for passthrough; nothing else needed
+defaults.list <- list(NODE_SHAPE = "ellipse", NODE_SIZE = 10, 
+                      NODE_FILL_COLOR = "#AAAAAA", EDGE_TRANSPARENCY = 120)
+# p for passthrough; nothing else needed
+node.label.map <- mapVisualProperty('node label', 'display name', 'p')
 createVisualStyle(style.name, defaults.list, list(node.label.map))
-setVisualStyle(style.name=style.name)
+setVisualStyle(style.name = style.name)
 
 # Create force directed layout
 NetworkImage <- function(){
@@ -82,11 +85,10 @@ NetworkImage()
   
 SaveNetworkImage <- function(){
   string_network_png_file_name <- file.path(getwd(), "string_network.png")
-  if(file.exists(string_network_png_file_name)){
+  if(file.exists(string_network_png_file_name)) {
   # cytoscape hangs waiting for user response if file already exists, so remove if present
-  response <- file.remove(string_network_png_file_name)
-  } else { 
-    response <- exportImage(string_network_png_file_name, type = "png") }
+  response <- file.remove(string_network_png_file_name) }
+  response <- exportImage(string_network_png_file_name, type = "png")
 }
 
 SaveNetworkImage()
