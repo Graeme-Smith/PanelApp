87416d4bf403884ad14b780c5fa9e46980bd91e6
Graeme Smith <graeme_c_smith@hotmail.com>
2019-12-05 10:16:40 +0000

    Added genome ref to function

--- webGestaltAPI.R
+++ webGestaltAPI.R
@@ -1,11 +1,20 @@
-
+library(WebGestaltR)
 # Interface with the WebGestalt API
 
-geneFile <- system.file("extdata", "interestingGenes.txt", package="WebGestaltR")
-refFile <- system.file("extdata", "referenceGenes.txt", package="WebGestaltR")
+geneFile <- system.file("extdata",
+                        "interestingGenes.txt",
+                        package="WebGestaltR")
+refFile <- system.file("extdata",
+                       "referenceGenes.txt",
+                       package="WebGestaltR")
 outputDirectory <- getwd()
-enrichResult <- WebGestaltR(enrichMethod="ORA", organism="hsapiens",
-                            enrichDatabase="pathway_KEGG", interestGeneFile=geneFile,
-                            interestGeneType="genesymbol", referenceGeneFile=refFile,
-                            referenceGeneType="genesymbol", isOutput=TRUE, outputDirectory=outputDirectory,
+enrichResult <- WebGestaltR(enrichMethod="ORA",
+                            organism="hsapiens",
+                            enrichDatabase="pathway_KEGG",
+                            interestGeneFile=geneFile,
+                            interestGeneType="genesymbol",
+                            referenceGeneFile=refFile,
+                            referenceGeneType="genesymbol",
+                            isOutput=TRUE,
+                            outputDirectory=outputDirectory,
                             projectName=NULL)
