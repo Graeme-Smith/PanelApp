ddc3c8b0b1fed133c79a5a7f376ef8322820dccd
Graeme Smith <graeme_c_smith@hotmail.com>
2019-12-05 12:38:31 +0000

    Removed unneeded code

--- webGestaltAPI.R
+++ webGestaltAPI.R
@@ -1,16 +1,6 @@
 library(WebGestaltR)
 # Interface with the WebGestalt API - uses the ORA enrichment method and "genome" reference set
 
-# Load in test data
-geneFile <- system.file("extdata",
-                        "interestingGenes.txt",
-                        package="WebGestaltR")
-myGeneList <-unlist(read.csv(geneFile, stringsAsFactors = FALSE))
-
-# Input: Gene list vector using gene symbols
-# Output: Data frame and HTML report 
-
-outputDirectory <- getwd()
 #TODO: auto generate projectName based upon input panel id 
 callWebGestalt <- function(myGeneList, outputDirectory) {
 enrichResult <- WebGestaltR(enrichMethod="ORA",
@@ -25,5 +15,3 @@ enrichResult <- WebGestaltR(enrichMethod="ORA",
                             projectName="temp_webGestalt")
   return(enrichResult)
 }
-# Currently hardcoded output directory (file written over every time app is run)
-html_temp_file <- paste0(outputDirectory, "/Project_temp_webGestalt/Report_temp_webGestalt.html")
